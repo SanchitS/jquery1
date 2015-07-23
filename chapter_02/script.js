e6b54978ec86f3229520abc04cfcc6e6e632b5da
sanchits <sanchitsharma1209@gmail.com>
2015-07-24 04:42:49 +0530

    1-2-3-4

---
+++ chapter_02/script.js
@@ -0,0 +1,32 @@
+////----------------------------------------------------------------------------------------------
+//ALL EXAMPLES ARE ASSEMBLED IN THIS SINGLE SCRIPT.JS FILE, 
+//TO PRACTISE THE PARTICULAR SNIPPET EITHER REMOVE THE COMMENTS OR WRITE BELOW THAT(do this, it will make your understanding well)
+//----------------------------------------------------------------------------------------------
+//
+//
+//
+
+//____________________________________chapter_02/01_document_ready/script.js
+//
+//$(document).ready(function(){
+//	alert('Welcome to StarTrackr! Now no longer under police investigation!');
+//});
+
+//____________________________________chapter_02/02_selecting/script.js
+//
+//$(document).ready(function() {
+//    alert($('#celebs tr').length + ' elements!');
+//});
+
+//____________________________________chapter_02/03_narrowing_selection/script.js
+//
+//$(document).ready(function () {
+//    alert($('#celebs tbody tr').length + " elements!");
+//});
+
+
+//____________________________________chapter_02/04_filters/script.js
+//
+//$(document).ready(function () {
+//    alert($('#celebs tbody tr:even').length + 'elements!');
+//});
